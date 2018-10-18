281b56aa3d8e377f349e85818f3f07f30f093fb7
Brian Young <by12380@gmail.com>
2018-10-18 14:32:06 -0700

    Hide navbar footer on login page

--- ClientApp/app/components/app/app.component.ts
+++ ClientApp/app/components/app/app.component.ts
@@ -2,6 +2,7 @@ import { VerseParserService } from './../../services/verse-parser.service';
 import { fade } from './../../animations/fade.animation';
 import { NavbarComponent } from './../navbar/navbar.component';
 import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app',
@@ -14,7 +15,11 @@ export class AppComponent {
     isLoading: boolean =  false;
     isVisible: boolean = false;
 
-    constructor(private verseParserService: VerseParserService) {
+    constructor(
+        private verseParserService: VerseParserService,
+        public router: Router
+    )
+    {
         this.verseParserService.isLoading.subscribe(isLoading => this.isLoading = isLoading);
     }
 
