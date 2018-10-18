1dd96431fd431890ac4f0bb6142d84dd3d1288ef
Brian Young <by12380@gmail.com>
2018-10-18 14:31:35 -0700

    Register component and service

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -3,7 +3,9 @@ import { VerseParserService } from './services/verse-parser.service';
 import { CounterComponent } from './components/counter/counter.component';
 import { NavFooterComponent } from './components/nav-footer/nav-footer.component';
 import { VerseContainerComponent } from './components/verse-container/verse-container.component';
+import { LoginComponent } from './components/login/login.component';
 import { StudySheetService } from './services/studysheet.service';
+import { AuthService } from './services/auth.service';
 import { StudysheetComponent } from './components/studysheet/studysheet.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgModule } from '@angular/core';
@@ -14,6 +16,10 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { environment } from './environments/environment';
 
 @NgModule({
     declarations: [
@@ -24,7 +30,8 @@ import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
         NavFooterComponent,
         StudysheetComponent,
         VerseContainerComponent,
-        CounterComponent
+        CounterComponent,
+        LoginComponent
     ],
     imports: [
         CommonModule,
@@ -34,15 +41,19 @@ import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent, data: { animation: 'home' } },
             { path: 'studysheet', component: StudysheetComponent, data: { animation: 'studysheet' } },
+            { path: 'login', component: LoginComponent },
             { path: 'versecontainer', component: VerseContainerComponent },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
-        ])
+        ]),
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireAuthModule
     ],
     providers: [
         StudySheetService,
         VerseParserService,
-        VerseService
+        VerseService,
+        AuthService
     ]
 })
 export class AppModuleShared {
---
+++ ClientApp/app/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyBRouWin41DtKgAvhuRg9Cp2mSVzIsrZnE",
+        authDomain: "instaverse-ac3e7.firebaseapp.com",
+        databaseURL: "https://instaverse-ac3e7.firebaseio.com",
+        projectId: "instaverse-ac3e7",
+        storageBucket: "instaverse-ac3e7.appspot.com",
+        messagingSenderId: "966637652843"
+    }
+};
