b39f00676b789178b407e6b38e9ec7b1d76a26b1
Brian Young <by12380@gmail.com>
2018-10-20 21:33:06 -0700

    Add get data from firebase

--- ClientApp/app/services/studysheet.service.ts
+++ ClientApp/app/services/studysheet.service.ts
@@ -1,9 +1,13 @@
+import { FirebaseApp } from 'angularfire2';
 import { VerseGroup } from './../models/VerseGroup';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class StudySheetService {
+
+    user: firebase.User;
 
     private verseGroups: VerseGroup[] = [];
     private verseGroupListSource = new BehaviorSubject<VerseGroup[]>(this.verseGroups);
@@ -13,16 +17,52 @@ export class StudySheetService {
     private isDeleteModeSource = new BehaviorSubject<boolean>(this._isDeleteMode);
     public isDeleteMode = this.isDeleteModeSource.asObservable();
 
-    constructor() { }
+    constructor(private auth: AuthService, private app: FirebaseApp) {
+        this.auth.user$.subscribe((user) => {
+            this.user = user;
+            if (this.user) {
+                this.app.database().ref(`/users/${user.uid}/verseGroups`).once('value', (snapShot) => {
+                    this.verseGroups = snapShot.val() ? snapShot.val() : [];
+                    this.verseGroupListSource.next(this.verseGroups);
+                })
+            } else {
+                this.verseGroups = [];
+                this.verseGroupListSource.next(this.verseGroups);
+            }
+        })
+    }
 
     addVerseGroup(verseGroup: VerseGroup) {
         this.verseGroups.push(verseGroup);
-        this.verseGroupListSource.next(this.verseGroups);
+
+        if (this.user) {
+            this._updateVerseGroupsAuthenticated();
+        }
+        else {
+            this._updateVerseGroupsGuest();
+        }
+
     }
 
     removeVerseGroup(verseGroup: VerseGroup){
         let index = this.verseGroups.indexOf(verseGroup);
         this.verseGroups.splice(index, 1);
+
+        if (this.user) {
+            this._updateVerseGroupsAuthenticated();
+        }
+        else {
+            this._updateVerseGroupsGuest();
+        }
+
+    }
+
+    _updateVerseGroupsGuest() {
+        this.verseGroupListSource.next(this.verseGroups);
+    }
+
+    _updateVerseGroupsAuthenticated() {
+        this.app.database().ref(`/users/${this.user.uid}/verseGroups`).set(this.verseGroups);
     }
 
     activateDeleteMode() {
